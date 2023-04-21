c41dee01fa17f2fc8155d25f694e175b09c4a4a9
Sushruth22 <62336924+Sushruth22@users.noreply.github.com>
2023-04-21 17:17:26 -0400

    LR-sample

--- sv.py
+++ sv.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import copy
 import os
-import pickle #dataframe save and load
+import pickle 
 import statistics
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -14,7 +14,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import cross_val_score
 #%%
-df = pd.read_pickle("./df_clean_min.pkl")
+df = pd.read_pickle("D:/Data_Mning_Final_Project-3/df_clean_min.pkl")
 #%%
 df.info()
 #%%
@@ -56,4 +56,61 @@ sns.boxenplot(data = df, x = 'instant_bookable', y  ='avail_365')
 
 #%%
 sns.boxenplot(data = df, x = 'instant_bookable', y  ='price')
+#%%
+cols_for_correlation = ['review_scores_rating','avail_365', 'accommodates', 'bedrooms', 'beds', 'price','availability', 'number_of_reviews', 'reviews_per_month']
 
+# Calculating Pearson correlation coefficients
+correlation_df = df[cols_for_correlation].corr(method='pearson')
+
+# Plotting correlation heatmap using seaborn
+sns.heatmap(correlation_df, annot=True, cmap='coolwarm', center=0)
+plt.title('Pearson Correlation Heatmap')
+plt.show()
+
+#%%
+df.info()
+#%%
+df['room_type'].unique()
+#%%
+from sklearn.preprocessing import LabelEncoder
+
+# Create an instance of LabelEncoder
+label_encoder = LabelEncoder()
+#%%
+# Apply label encoding to 'room_type' column
+df['room_type_encoded'] = label_encoder.fit_transform(df['room_type'])
+#%%
+#ONE HOT ENCODING
+df = pd.get_dummies(df, columns=['room_type'], prefix='dm')
+#%%
+df['dm_Entire home/apt']
+#%%
+df.shape
+#%%
+df.isna().sum()
+#%%
+df = df.dropna()
+#%%
+df.isnull().sum()
+#%%
+sns.displot(data = df, x ='avail_365')
+plt.show()
+#%%
+sns.displot(data = df, x ='price')
+plt.show()
+#%%
+X = df.drop(['review_scores_rating_t2','listing_id'], axis=1) #All input
+
+ip=['bedrooms','beds','price','min_nights','max_nights','availability','avail_365','number_of_reviews','Avg_neg_review_comment_score','Avg_pos_review_comment_score','Avg_neu_review_comment_score']
+#%%
+X = X[ip]
+y = df['review_scores_rating_t2']
+#%%
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.3, random_state=42)
+model = LogisticRegression(max_iter=300)
+model_fit = model.fit(X_train, y_train)
+#%%
+
+print('model accuracy for test', model_fit.score(X_test, y_test))
+print('model accuracy for train', model_fit.score(X_train, y_train))
+#%%
